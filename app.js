88d5c1628b4739a9c4d0b18535677bb537eb6356
Inês Almeida <ines_almeida_203@hotmail.com>
2023-05-23 12:42:07 +0100

    until iteration 3

--- app.js
+++ app.js
@@ -13,13 +13,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  const allBeers = punkAPI.getBeers()
+  allBeers.then(beer => {
+  res.render('beers', {beer});
+  })
+  allBeers.catch((err => {
+    res.send('Error');
+  }))
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
